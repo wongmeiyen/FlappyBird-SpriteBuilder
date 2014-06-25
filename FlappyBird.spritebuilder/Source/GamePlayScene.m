d7ba82d81502d91dbad02e81adf1f490af699268
Melissa Wong <wongmeiyen@Melissas-Mac-mini.local>
2014-06-24 23:39:43 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -15,10 +15,11 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
-    timeSinceObstacle += delta;
-    if (timeSinceObstacle > 2.0f)
+    
     [self addObstacle];
     timeSinceObstacle = 0.0f;
+    timeSinceObstacle += delta;
+    if (timeSinceObstacle > 2.0f)
 }
 
 // put new methods here
