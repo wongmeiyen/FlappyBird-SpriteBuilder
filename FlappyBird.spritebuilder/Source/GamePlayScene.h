f5489a60940715e90aa8d9d825850c96dcfad9f5
Melissa Wong <wongmeiyen@Melissas-Mac-mini.local>
2014-06-24 23:38:09 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -20,6 +20,7 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
     // define variables here;
     Character*     character;
     CCPhysicsNode* physicsNode;
+    timeSinceObstacle = 0.0f;
 }
 
 -(void) initialize;
